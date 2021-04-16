53d832c9b874dc502318fe0d9455189192bdebf6
Maarten86M <maartenmoelands@gmail.com>
2021-04-16 12:53:44 +0200

    functie geschreven

---
+++ index.js
@@ -0,0 +1,9 @@
+// functie schrijven.
+
+function getSquare(length, width, height) {
+    console.log("input", length, width, height);
+    const result = length * width * height;
+    console.log("result" ,result);
+    return result;
+}
+
